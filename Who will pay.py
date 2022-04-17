fe1b344e474cbf486b38032735bb4db761a6ac4e
mkabir88 <95720420+mkabir88@users.noreply.github.com>
2022-04-18 01:18:49 +0300

    v2 push

--- Who will pay.py
+++ Who will pay.py
@@ -9,3 +9,5 @@ number_of_person = (len(names))
 random_choice = random.randint(0, number_of_person - 1)
 person_who_will_pay = names[random_choice]
 print(person_who_will_pay + " is going to pay for the dinner today!! :D")
+
+#this is v2
